d42153e01df9b38e643111b49d32b654b9f29f0a
nsgomezm <nicolsteveengomesmarin@gmail.com>
2023-11-23 15:59:52 -0500

    first commit

---
+++ src/layouts/components/TemplateFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+      <h1>footer</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/layouts/components/TemplateHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+      <h1>header</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
